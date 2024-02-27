d9b74952b2eeef63f52bc175f619c7b9759fadc0
sam6701 <92205751+sam6701@users.noreply.github.com>
2024-02-27 17:15:51 +0530

    try

---
+++ lib/colors/color_utils.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class ColorUtils {
+  static const jungleGreen = Color(0xff20A986);
+  static const jungleGreen2 = Color(0xff1DAB87);
+  static const lightcream = Color(0xffFCC17C);
+  static const darkbrownish = Color(0xffC35810);
+  static const darkbrownish2 = Color(0xff8F3D00);
+  static const lightbrownishpink = Color(0xffFCEBD8);
+  static const dascolor = Color(0xffC35810);
+  static const trefcolor = Color(0xff6B6B6B);
+  static const nocolor = Color(0xff151515);
+  static const lightGreygmail = Color(0xff6B6B6B);
+  static const lightGreytext = Color(0xff525252);
+
+  static const primary = Color(0xffD4600B);
+  static const lightOrange = Color(0xffFCC17C);
+  static const pinkFc = Color(0xffD4600B);
+  static const blue = Color(0xff4E54FF);
+
+  static const jungleGreen3 = Color(0xff40957F);
+  static const lapisBlue = Color(0xff1D3A70);
+  static const paleSky = Color(0xff6B7280);
+  static const alabaster = Color(0xffF9FAFB);
+  static const santaGrey = Color(0xff9CA3AF);
+  static const mercury = Color(0xffE5E7EB);
+  static const dune = Color(0xff313131);
+  static const lightGrey = Color(0xffD9D9D9);
+  static const white = Colors.white;
+  static Color whiteWithOpacity = Colors.white.withOpacity(0.1);
+  static const blackColor = Colors.black;
+  static const black = Colors.black;
+  static const pacificBlue = Color(0xff00A9C1);
+  static const lightPink = Color(0xffD88EA9);
+  static const green = Color(0xff86EC27);
+  static const darkGrey = Color(0xff65656B);
+  static const cream = Color(0xffC4C4C4);
+  static const black0E = Color(0xff000C0E);
+  static const transparent = Colors.transparent;
+  static const grey5B = Color(0xff44475B);
+  static const red = Color(0xffFF2C2C);
+  static const lightGrey2 = Color(0xff959595);
+  static const grey61 = Color(0xff616161);
+  static const lightModeGrey = Color(0xff7F909F);
+  static const lightimgGrey = Color(0xffD9D9D9);
+  static const lightextGrey = Color(0xff939393);
+  static const darktextGrey = Color(0xff151515);
+
+  static const Color menuBackground = Color(0xFF090912);
+  static const Color itemsBackground = Color(0xFF1B2339);
+  static const Color pageBackground = Color(0xFF282E45);
+  static const Color mainTextColor1 = Colors.white;
+  static const Color mainTextColor2 = Colors.white70;
+  static const Color mainTextColor3 = Colors.white38;
+  static const Color mainGridLineColor = Colors.white10;
+  static const Color borderColor = Colors.white54;
+  static const Color gridLinesColor = Color(0x11FFFFFF);
+
+  static const Color contentColorBlack = Colors.black;
+  static const Color contentColorWhite = Colors.white;
+  static const Color contentColorBlue = Color(0xFF2196F3);
+  static const Color contentColorYellow = Color(0xFFFFC300);
+  static const Color contentColorOrange = Color(0xFFFF683B);
+  static const Color contentColorGreen = Color(0xFF3BFF49);
+  static const Color contentColorPurple = Color(0xFF6E1BFF);
+  static const Color contentColorPink = Color(0xFFFF3AF2);
+  static const Color contentColorRed = Color(0xFFE80054);
+  static const Color contentColorCyan = Color(0xFF50E4FF);
+}
